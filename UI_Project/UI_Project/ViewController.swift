c32dae30b5348d9abeddf807dea8e87dc1499722
Shisetsu <shisetsu@me.com>
2020-12-03 00:07:09 +0300

    project created

---
+++ UI_Project/UI_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI_Project
+//
+//  Created by  Ilya Dunaev on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
